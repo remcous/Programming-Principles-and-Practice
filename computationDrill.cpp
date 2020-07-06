5fc12fa03f19acdd3fb0fd4fdb333766f5589845
rem11 <remcous@gmail.com>
2020-07-05 21:39:54 -0400

    First Commit

---
+++ computationDrill.cpp
@@ -0,0 +1,85 @@
+#include "std_lib_facilities.h"
+
+// No magic numbers: conversion factors
+constexpr double CM_TO_M = 1.0/100;
+constexpr double IN_TO_M = 2.54 * CM_TO_M;
+constexpr double FT_TO_M = 12 * IN_TO_M;
+
+int main(){
+    // input variables
+    double number;
+    string unit;
+    vector<double> numbers;
+
+    // analytics of numbers
+    double largest, smallest, sum=0;
+    int count = 0;
+
+    // flag to initialize the largest and smallest values
+    bool first_run = true;
+
+    cout << "Enter a measurement: ";
+
+    while(cin >> number >> unit){
+        // unit check and conversion
+        if(unit == "m");
+        else if(unit == "cm"){
+            number *= CM_TO_M;
+        }else if(unit == "in"){
+            number *= IN_TO_M;
+        }else if(unit == "ft"){
+            number *= FT_TO_M;
+        }else{  // error handling
+            cout << "Sorry, I don't know the unit: " << unit << endl;
+            cout << "Enter a measurement: ";
+            continue;
+        }
+
+        // output the number in terms of meters
+        cout << number << "m";
+        count++;
+        sum += number;
+        numbers.push_back(number);
+
+        // handle special case of first run
+        if(first_run){
+            largest = number;
+            smallest = number;
+            first_run = false;
+        }
+
+        // check for new largest or smallest value
+        if(number > largest){
+            cout << " the largest so far";
+            largest = number;
+        }
+
+        if (number < smallest){
+            cout << " the smallest so far";
+            smallest = number;
+        }
+
+        cout << endl;
+        cout << "Enter a measurement: ";
+    }
+
+    cout << "\n\n";
+
+    sort(numbers);
+
+    for(double num : numbers){
+        cout << num << endl;
+    }
+
+    if(count > 0){
+        cout << "\n\nThe largest value is: " << largest << "m\n";
+        cout << "The smallest value is " << smallest << "m\n";
+        if(count > 1){
+            cout << "The " << count 
+                << " numbers sum up to " << sum << "m\n";
+        }
+        else{
+            cout << "The number is " << sum << "m\n";
+        }
+    }
+}
---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,29 @@
+#include "std_lib_facilities.h"
+
+#define MAX_NUM 100
+
+int main(){
+    bool numbers[MAX_NUM + 1];
+
+    numbers[0] = false;
+    numbers[1] = false;
+
+    for(int i=2; i<=MAX_NUM; i++){
+        numbers[i] = true;
+    }
+
+    for(int i=2; i<=MAX_NUM; i++){
+        if(!numbers[i]) continue;
+
+        for(int j=i*2; j<=MAX_NUM; j+=i){
+            numbers[j] = false;
+        }
+    }
+
+    for(int i=2; i<=MAX_NUM; i++){
+        cout << i << ' ';
+    }
+
+    cout << endl;
+    return 0;
+}
