70889f07d7ca1def5ed78487717cc903926f19a9
rem11 <remcous@gmail.com>
2020-07-08 00:22:38 -0400

    Chapter 6 calculator, before exercises

--- Ch6/calculator.cpp
+++ Ch6/calculator.cpp
@@ -6,6 +6,10 @@
 
 #include "../std_lib_facilities.h"
 
+/****************************************
+*           CLASS DEFINITIONS
+****************************************/
+
 class Token {
 public:
     char kind;        // what kind of token
@@ -16,65 +20,78 @@ public:
         :kind(ch), value(val) { }
 };
 
-Token get_token()    // read a token from cin
-{
-    char ch;
-    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+class Token_stream{
+public:
+    Token_stream();         // make a Token_stream that reads from cin
+    Token get();            // get a Token
+    void putback(Token t);  // put a Token back
+private:
+    bool full = false;       // is there a Token in the buffer?
+    Token buffer;           // here is where we keep a Token put back using putback()
+};
 
-    switch (ch) {
-    case '(': case ')': case '+': case '-': case '*': case '/': 
-        return Token(ch);        // let each character represent itself
-    case '.':
-    case '0': case '1': case '2': case '3': case '4':
-    case '5': case '6': case '7': case '8': case '9':
-        {    
-            cin.putback(ch);         // put digit back into the input stream
-            double val;
-            cin >> val;              // read a floating-point number
-            return Token('8',val);   // let '8' represent "a number"
-        }
-    default:
-        error("Bad token");
-    }
-}
+/****************************************
+*           FUNCTION PROTOTYPES
+****************************************/
 
 double expression();    // deal with + and -
 double term();          // deal with *, /, and %
 double primary();       // deal with numbers and parentheses
 
+/****************************************
+*           GLOBAL VARIABLES
+****************************************/
+
+Token_stream ts;    // Global Token stream
+
+/****************************************
+*             MAIN FUNCTION
+****************************************/
+
 int main(){
     try{
-        while(cin)
-            cout << "=" << expression() << '\n';
-        keep_window_open();
+        double val = 0;
+        while(cin){
+            Token t = ts.get();
+
+            if(t.kind == 'q') break;    // 'q' for quit
+            if(t.kind == ';')           // ';' for print now
+                cout << "=" << val << '\n';
+            else
+                ts.putback(t);
+            val = expression();
+        }
     }
     catch(exception& e){
         cerr << e.what() << '\n';
-        keep_window_open();
         return 1;
     }
     catch(...){
         cerr << "exception\n";
-        keep_window_open();
         return 2;
     }
 }
 
+/****************************************
+*       FUNCTION DEFINITIONS - GLOBAL
+****************************************/
+
 double expression(){
     double left = term();       // read and evaluate as a Term
-    Token t = get_token();      // get the next token
+    Token t = ts.get();         // get the next token from Token_stream
 
     while(true){
         switch(t.kind){
         case '+':
             left += term();     // evaluate Term and add
-            t = get_token();
+            t = ts.get();
             break;
         case '-':
             left -= term();     // evaluate Term and subtract
-            t = get_token();
+            t = ts.get();
             break;
         default:
+            ts.putback(t);      // put t back into the token stream
             return left;        // finally: no more + or -; return the answer
         }
     }
@@ -82,34 +99,35 @@ double expression(){
 
 double term(){
     double left = primary();
-    Token t = get_token();
+    Token t = ts.get();         // get the next token from Token stream
     while(true){
         switch(t.kind){
         case '*':
             left *= primary();
-            t = get_token();
+            t = ts.get();
             break;
         case '/':
         {
             double d = primary();
             if(d == 0) error("divide by zero");
             left /= d;
-            t = get_token();
+            t = ts.get();
             break;
         }
         default:
+            ts.putback(t);      // put t back into the Token stream
             return left;
         }
     }
 }
 
 double primary(){
-    Token t = get_token();
+    Token t = ts.get();
     switch(t.kind){
     case '(':   // Handle '(' expression ')'
     {
         double d = expression();
-        t = get_token();
+        t = ts.get();
         if(t.kind != ')') error("')' expected");
         return d;
     }
@@ -117,5 +135,46 @@ double primary(){
         return t.value; // return the number's value
     default:
         error("primary expected");
+    }
+}
+
+/****************************************
+*   FUNCTION DEFINITIONS - TOKEN_STREAM
+****************************************/
+
+Token_stream::Token_stream():full(false),buffer(0)  // no token in buffer
+{}
+
+void Token_stream::putback(Token t){
+    if(full) error("putback() into a full buffer");
+    buffer = t;     // copy t to buffer
+    full = true;    // buffer is now full
+}
+
+Token Token_stream::get(){
+    if(full){           // do we already have a Token ready?
+        full = false;   // remove Token from buffer
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;          // note that >> skips whitespace(space, newline, tab, etc)
+
+    switch(ch){
+    case ';':   // for print
+    case 'q':   // for quit
+    case '(': case ')': case '+': case '-': case '*': case '/':
+        return Token{ch};   // let each character represent itself
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '8': case '9':
+    {
+        cin.putback(ch);        // put digit back into the input stream
+        double val;
+        cin >> val;             // read a floating-point number
+        return Token('8',val);  // let '8' represent "a number"
+    }
+    default:
+        error("Bad token");
     }
 }
