5d6704bdb069f62dacb2ba1153d1476a3879127f
rem11 <remcous@gmail.com>
2020-07-13 14:49:52 -0400

    Chapter 8 drill 1 complete

---
+++ Ch8/my.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+#include "../std_lib_facilities.h"
+
+int foo;
+
+void print_foo(){
+	std::cout << foo;
+}
+
+void print(int i){
+	std::cout << i;
+}
---
+++ Ch8/use.cpp
@@ -0,0 +1,7 @@
+#include "my.h"
+
+int main(){
+	foo = 7;
+	print_foo();
+	print(99);
+}
