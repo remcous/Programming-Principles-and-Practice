030d994b0a1556e9f33f589f4083bc180570ff58
rem11 <remcous@gmail.com>
2020-07-14 01:08:23 -0400

    Updated Date.cpp to overload ++ and << operators

--- Ch9/Date.cpp
+++ Ch9/Date.cpp
@@ -4,32 +4,58 @@
 *			CLASS/STRUCT DEFINITIONS
 ***********************************************/
 
-// Simple Date (too simple?)
-struct Date{
-	int y;	// year
-	int m;	// month in year
-	int d;	// day of month
+// Simple Date (some people prefer implementation details last)
+class Date{
+public:
+	class Invalid{};			// to be used as exception
+	Date(int y, int m, int d);	// check for valid date and initialize
+	void add_day(int n);		// increase the Date by n days
+	int month(){return m;}
+	int day(){return d;}
+	int year(){return y;}
+private:
+	int y,m,d;			// year, month, day
+	bool is_valid();	// return true if date is valid
+};
+
+enum class Month{
+	jan=1,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec
+};
+
+enum class Day{
+	monday,tuesday,wednesday,thursday,friday,saturday,sunday
 };
 
 /***********************************************
 *				GLOBAL VARIABLES
 ***********************************************/
 
-Date today;	// Date variable (a named object)
+vector<string> month_tbl = {
+	"", "January", "February", "March", "April", 
+	"May", "June", "July", "August", "September",
+	"October", "November", "December"
+};
 
 /***********************************************
 *				FUNCTION PROTOTYPES
 ***********************************************/
 
-void init_day(Date&, int y, int m, int d);
-void add_day(Date& dd, int n);
-void f();
+Month int_to_month(int x);
+Month operator++(Month& m);
+ostream& operator<<(ostream& os, Month m);
 
 /***********************************************
 *				  MAIN PROGRAM
 ***********************************************/
 
 int main(){
+	Month m = Month::sep;
+
+	cout << ++m << endl;
+	cout << ++m << endl;
+	cout << ++m << endl;
+	cout << ++m << endl;
+
 	return 0;
 }
 
@@ -37,17 +63,36 @@ int main(){
 *				GLOBAL FUNCTIONS
 ***********************************************/
 
-void init_day(Date& dd, int y, int m, int d){
-	// check that (y,m,d) is a valid date
-	// if it is use it to initialize dd
+Month int_to_month(int x){
+	if(x<int(Month::jan)||int(Month::dec)<x) error("bad month");
+	return Month(x);
 }
 
-void add_day(Date& dd, int n){
-	// increase dd by n days
+Month operator++(Month& m){
+	m = (m==Month::dec) ? Month::jan : Month(int(m)+1);	// wrap around
+	return m;
 }
 
-void f(){
-	Date today;
-	init_day(today, 12, 24, 2005);
-	add_day(today, 1);
+ostream& operator<<(ostream& os, Month m){
+	return os << month_tbl[int(m)];
+}
+
+/***********************************************
+*			  DATE MEMBER FUNCTIONS
+***********************************************/
+
+Date::Date(int yy, int mm, int dd)	// Constructor
+	:y{yy}, m{mm}, d{dd}
+{
+	if(!is_valid()) throw Invalid{};
+}
+
+void Date::add_day(int n){
+	//	...
+}
+
+bool Date::is_valid(){
+	if(m<1||m>12) return false;
+	// ...
+	return true;
 }
