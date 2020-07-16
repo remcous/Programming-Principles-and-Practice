aec8093408ca3281423625980c570206bdd83db0
rem11 <remcous@gmail.com>
2020-07-15 21:45:18 -0400

    Chapter 9 Exercise 6 support added for ==, !=, and << operators for Book class

--- Ch9/Exercise5.cpp
+++ Ch9/Exercise5.cpp
@@ -2,8 +2,9 @@
 *	Exercise 5: design and implement a Book class
 *	By: Remi Coussement
 *	Rev:
+*		Exercise 6 - operator support for ==, !=, <<
 *		Exercise 5 - Book holds ISBN, title, author, 
-			copyright date - July 2020
+*			copyright date - July 2020
 ******************************************************/
 #include "../std_lib_facilities.h"
 
@@ -15,13 +16,13 @@ class Book{
 public:
 	Book();
 	Book(string isbn, string title, string author, int copyright, bool checked_out);
-	string get_isbn(){return isbn;}
-	string get_title(){return title;}
-	string get_author(){return author;}
-	int get_copyright(){return copyright;}
-	bool get_checked_out(){return checked_out;}
+	string get_isbn() const{return isbn;}
+	string get_title() const{return title;}
+	string get_author() const{return author;}
+	int get_copyright() const{return copyright;}
+	bool get_checked_out() const{return checked_out;}
 private:
-	bool valid_isbn(string isbn);
+	bool valid_isbn(string test_isbn) const;
 	string isbn;
 	string title;
 	string author;
@@ -33,17 +34,44 @@ private:
 *		  FUNCTION PROTOTYPE
 **********************************/
 
+bool operator==(const Book& a, const Book& b);
+bool operator!=(const Book& a, const Book& b);
+ostream& operator<<(ostream& os, const Book& book);
+
 /**********************************
 *			MAIN PROGRAM
 **********************************/
 
 int main(){
 	try{
-		string isbn;
-		cout << "Enter ISBN: ";
-		cin >> isbn;
-		Book(isbn, "PPP", "Bjarne Stroustrup", 2014, false);
+		Book book1{};
+		Book book2{"0-321-99278-4","Programming Principles and Practices using C++ 2e", "Bjarne Stroustrup", 2014, true};
+		Book book3{};
+		
+		
+		// test ==
+		cout << "TESTING == OPERATOR:\n";
+		if(book1 == book2){
+			cerr << "Book and 2 shouldn't match but do\n";
+		}else cout << "Book 1 and 2 are not the same!\n";
+		
+		if(book1 == book3){
+			cout << "Book 1 and 3 are the same!\n";
+		}else cerr << "Book 1 and 3 should match but don't\n";
+		
+		// test !=
+		cout << "\n\nTESTING != OPERATOR:\n";
+		if(book1 != book2){
+			cout << "Book 1 and 2 are not the same!\n";
+		}else cerr << "Book and 2 shouldn't match but do\n";
 
+		if(book1 != book3){
+			cerr << "Book 1 and 3 should match but don't\n";
+		}else cout << "Book 1 and 3 are the same!\n";
+		
+		cout << "\n\nTESTING << OPERATOR:\n";
+		cout << book2;
+		
 		return 0;
 	}catch(exception& e){
 		cerr << "Error: " << e.what() << "\n";
@@ -76,20 +104,20 @@ Book::Book(string isbn, string title, string author, int copyright, bool checked
 	this->checked_out = checked_out;
 }
 
-bool Book::valid_isbn(string isbn){
+bool Book::valid_isbn(string test_isbn) const{
 	// test for valid ISBN
 	int count = 0;
 	bool previous_digit = false;
 
-	for(int i=0; i<isbn.length(); i++){
+	for(int i=0; i<test_isbn.length(); i++){
 		if(previous_digit){
-			if(isdigit(isbn[i])) continue;
+			if(isdigit(test_isbn[i])) continue;
 			previous_digit = false;
-			if(isbn[i] != '-') return false;
+			if(test_isbn[i] != '-') return false;
 		}else{
-			if(count == 3 && (isalpha(isbn[i] || isdigit(isbn[i]))))
+			if(count == 3 && (isalpha(test_isbn[i] || isdigit(test_isbn[i]))))
 				count++;
-			else if(isdigit(isbn[i])){
+			else if(isdigit(test_isbn[i])){
 				count++;
 				previous_digit = true;
 			}else return false;
@@ -103,3 +131,20 @@ bool Book::valid_isbn(string isbn){
 /**********************************
 *		   GLOBAL FUNCTIONS
 **********************************/
+
+bool operator==(const Book& a, const Book& b){
+	int num = a.get_isbn().compare(b.get_isbn());
+	return num == 0 ? true : false;
+}
+
+bool operator!=(const Book& a, const Book& b){
+	return !(a == b);
+}
+
+
+ostream& operator<<(ostream& os, const Book& book){
+	os << "Title: " << book.get_title()
+		<< "\nAuthor: " << book.get_author()
+		<< "\nISBN: " << book.get_isbn() << endl;
+	return os;
+}
